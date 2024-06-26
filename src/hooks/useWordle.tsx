c41fa111a44b212134a63673fb274d5f517c1c97
Lenovo <talantbekovasezimai@github.com>
2024-06-26 15:13:20 +0600

    bug2

--- src/hooks/useWordle.tsx
+++ src/hooks/useWordle.tsx
@@ -1,91 +1,101 @@
-import { useState, useEffect } from "react";
-import useFetch from "./useFetch";
+import { useState, useEffect } from 'react'
+import useFetch from '../Tile'
 
 export const useWordle = () => {
-  const [guesses, setGuesses] = useState<string[][]>(Array.from({ length: 6 }, () => Array(5).fill("")));
-  const [currentGuesses, setCurrentGuesses] = useState<string>("");
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [gameOver, setGameOver] = useState<boolean>(false);
-  const { isLoading, data } = useFetch();
-  const [word, setWord] = useState<string>("");
+	const [guesses, setGuesses] = useState<string[][]>(
+		Array.from({ length: 6 }, () => Array(5).fill(''))
+	)
+	const [currentGuesses, setCurrentGuesses] = useState<string>('')
+	const [currentIndex, setCurrentIndex] = useState<number>(0)
+	const [gameOver, setGameOver] = useState<boolean>(false)
+	const [messsage, setMessage] = useState<boolean>(false)
+	const { isLoading, data } = useFetch()
+	const [word, setWord] = useState<string>('')
 
-  useEffect(() => {
-    if ( data.length > 0) {
-      initializeGame();
-    }
-  }, [data]);
+	useEffect(() => {
+		if (data.length > 0) {
+			initializeGame()
+		}
+	}, [data])
 
-  const initializeGame = () => {
-    if ( data.length > 0) {
-      const randomIndex = Math.floor(Math.random() *  data.length);
-      const randomWord =  data[randomIndex];
-      console.log("Случайно выбранное слово:", randomWord);
-      setWord(randomWord);
-      setGuesses(Array.from({ length: 6 }, () => Array(5).fill("")));
-      setCurrentGuesses("");
-      setCurrentIndex(0);
-      setGameOver(false);
-    }
-  };
+	const initializeGame = () => {
+		if (data.length > 0) {
+			const randomIndex = Math.floor(Math.random() * data.length)
+			const randomWord = data[randomIndex]
+			console.log('Случайно выбранное слово:', randomWord)
+			setWord(randomWord)
+			setGuesses(Array.from({ length: 6 }, () => Array(5).fill('')))
+			setCurrentGuesses('')
+			setCurrentIndex(0)
+			setGameOver(false)
+			setMessage(false)
+		}
+	}
 
-  const handleKeyPress = (e: KeyboardEvent) => {
-    if (gameOver) return;
+	const handleKeyPress = (e: KeyboardEvent) => {
+		if (gameOver) return
 
-    if (e.key === "Enter" && currentGuesses.length === 5) {
-      if (currentGuesses === word) {
-        alert(`Вы угадали слово ${word}`);
-        setGameOver(true);
-      } else {
-        if (currentIndex < 5) {
-          setCurrentIndex(currentIndex + 1);
-          setCurrentGuesses("");
-        } else {
-          alert(`Вы проиграли. Правильное слово: ${word}`);
-          setGameOver(true);
-        }
-      }
-    } else if (e.key === "Backspace") {
-      setCurrentGuesses(currentGuesses.slice(0, -1));
-    } else if (/^[a-zA-Z]$/.test(e.key) && currentGuesses.length < 5) {
-      setCurrentGuesses(currentGuesses + e.key.toUpperCase());
-    }
-  };
+		if (e.key === 'Enter' && currentGuesses.length === 5) {
+			if (currentGuesses === word) {
+				alert(`Вы угадали слово ${word}`)
+				setGameOver(true)
+				setMessage(false)
+			} else {
+				if (currentIndex < 5) {
+					setCurrentIndex(currentIndex + 1)
+					setCurrentGuesses('')
+				} else {
+					alert(`Вы проиграли. Правильное слово: ${word}`)
+					setGameOver(true)
+					setMessage(true)
+				}
+			}
+		} else if (e.key === 'Backspace') {
+			setCurrentGuesses(currentGuesses.slice(0, -1))
+		} else if (/^[a-zA-Z]$/.test(e.key) && currentGuesses.length < 5) {
+			setCurrentGuesses(currentGuesses + e.key.toUpperCase())
+		}
+	}
 
-  useEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => handleKeyPress(e);
-    document.addEventListener("keydown", handleKeyDown);
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [currentGuesses, currentIndex, gameOver]);
+	useEffect(() => {
+		const handleKeyDown = (e: KeyboardEvent) => handleKeyPress(e)
+		document.addEventListener('keydown', handleKeyDown)
+		return () => {
+			document.removeEventListener('keydown', handleKeyDown)
+		}
+	}, [currentGuesses, currentIndex, gameOver])
 
-  useEffect(() => {
-    const newGuesses = [...guesses];
-    newGuesses[currentIndex] = currentGuesses.split("");
-    setGuesses(newGuesses);
-  }, [currentGuesses, currentIndex]);
+	useEffect(() => {
+		const newGuesses = [...guesses]
+		newGuesses[currentIndex] = currentGuesses.split('')
+		setGuesses(newGuesses)
+	}, [currentGuesses, currentIndex])
 
-  const getTileClass = (letter: string, col: number, row: number) => {
-    if (gameOver && guesses[row].join('') === word) {
-      return "correct";
-    }
-    if (word[col] === letter) {
-      return "correct";
-    }
-    if (word.includes(letter)) {
-      return "present";
-    }
-    return "incorrect";
-  };
+	const getTileClass = (letter: string, col: number, row: number) => {
+		if (gameOver && guesses[row].join('') === word) {
+			return 'correct'
+		}
+		if (word[col] === letter) {
+			return 'correct'
+		}
+		if (word.includes(letter) && letter == '') {
+			return 'white'
+		}
+		if (word.includes(letter) && letter) {
+			return 'present'
+		}
+		return 'incorrect'
+	}
 
-  return {
-    word,
-    isLoading,
-    guesses,
-    gameOver,
-    currentGuesses,
-    currentIndex,
-    getTileClass,
-    initializeGame,
-  };
-};
+	return {
+		word,
+		isLoading,
+		guesses,
+		gameOver,
+		currentGuesses,
+		currentIndex,
+		getTileClass,
+		initializeGame,
+		messsage
+	}
+}
--- src/pages/WordG.tsx
+++ src/pages/WordG.tsx
@@ -1,52 +1,64 @@
-import React from "react";
-import { Tile } from "../components";
-import { useWordle } from "../hooks/useWordle";
-import "./WordG.css";
+import React from 'react'
+import { useWordle } from '../hooks'
+import { Tile } from '../components'
+import './WordG.css'
 
 const WordG = () => {
-  const {
-    word,
-    isLoading,
-    guesses,
-    gameOver,
-    currentGuesses,
-    currentIndex,
-    getTileClass,
-    initializeGame,
-  } = useWordle();
-  // console.log(word);
+	const {
+		word,
+		isLoading,
+		messsage,
+		guesses,
+		gameOver,
+		currentGuesses,
+		currentIndex,
+		getTileClass,
+		initializeGame
+	} = useWordle()
+	console.log(word)
 
-  return (
-    <div className="word-container">
-      {isLoading ? (
-        <p>Загрузка...</p>
-      ) : (
-        <div>
-          {Array.from({ length: 6 }, (_, row) => (
-            <div key={row} className="row">
-              {Array.from({ length: 5 }, (_, col) => {
-                const index = row * 5 + col;
-                const inputValue = guesses[row][col] || "";
-                const tileClass =
-                  currentIndex > row ?  `${getTileClass (inputValue, col, row)} ` : "";
-                return (
-                  <Tile 
-                    key={index}
-                    index={index}
-                    inputValue={inputValue}
-                    tileClass={tileClass}
-                  />
-                );
-              })}
-            </div>
-          ))}
-          {gameOver && (
-            <button onClick={initializeGame}>Начать заново</button>
-          )}
-        </div>
-      )}
-    </div>
-  );
-};
+	return (
+		<div className='word-container'>
+			<h1>Wordle Games</h1>
+			{isLoading ? (
+				<p>Загрузка...</p>
+			) : (
+				<div>
+					{Array.from({ length: 6 }, (_, row) => (
+						<div key={row} className='row'>
+							{Array.from({ length: 5 }, (_, col) => {
+								const index = row * 5 + col
+								const inputValue = guesses[row][col] || ''
+								const tileClass =
+									currentIndex > row || gameOver
+										? getTileClass(inputValue, col, row)
+										: ''
+								return (
+									<Tile
+										key={index}
+										index={index}
+										inputValue={inputValue}
+										tileClass={tileClass}
+									/>
+								)
+							})}
+						</div>
+					))}
+					<div className="you-lost">
 
-export default WordG;
+					{messsage && (
+						<p style={{ color: 'black' }}>Правильное слово: {word}</p>
+					)}
+					{gameOver && (
+						<div>
+							<button onClick={initializeGame}>Начать заново</button>
+						</div>
+					)}
+					</div>
+				</div>
+			)}
+		</div>
+	)
+}
+
+export default WordG
